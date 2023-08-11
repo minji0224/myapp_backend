8ab7278cc4f33db1cbb3d40125009082f81675af
Minji Choi <minj22222c@gmail.com>
2023-08-11 12:05:25 +0900

    first commit

---
+++ src/test/java/com/cmj/myapp/MyappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmj.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
